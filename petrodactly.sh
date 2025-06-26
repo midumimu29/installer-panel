1b5851c1f2e080b9dced259ca0fc68b1bc0952d0
Midu <midumimu29@gmail.com>
2025-06-26 15:11:07 +0000

    Tambahkan skrip instalasi panel

---
+++ petrodactly.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Midu Dev
+
+echo " Installing Pterodactyl Panel Menggunakan Docker..."
+
+# Step 1: Buat struktur directory
+mkdir -p pterodactyl/panel
+cd pterodactyl/panel || exit
+
+# Step 2: Buat file docker-compose.yml
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+x-common:
+  database:
+    &db-environment
+    MYSQL_PASSWORD: &db-password "CHANGE_ME"
+    MYSQL_ROOT_PASSWORD: "CHANGE_ME_TOO"
+  panel:
+    &panel-environment
+    APP_URL: "https://pterodactyl.example.com"
+    APP_TIMEZONE: "UTC"
+    APP_SERVICE_AUTHOR: "noreply@example.com"
+    TRUSTED_PROXIES: "*"
+  mail:
+    &mail-environment
+    MAIL_FROM: "noreply@example.com"
+    MAIL_DRIVER: "smtp"
+    MAIL_HOST: "mail"
+    MAIL_PORT: "1025"
+    MAIL_USERNAME: ""
+    MAIL_PASSWORD: ""
+    MAIL_ENCRYPTION: "true"
+
+services:
+  database:
+    image: mariadb:10.5
+    restart: always
+    command: --default-authentication-plugin=mysql_native_password
+    volumes:
+      - "/srv/pterodactyl/database:/var/lib/mysql"
+    environment:
+      <<: *db-environment
+      MYSQL_DATABASE: "panel"
+      MYSQL_USER: "pterodactyl"
+
+  cache:
+    image: redis:alpine
+    restart: always
+
+  panel:
+    image: ghcr.io/pterodactyl/panel:latest
+    restart: always
+    ports:
+      - "80:80"
+      - "443:443"
+    links:
+      - database
+      - cache
+    volumes:
+      - "/srv/pterodactyl/var/:/app/var/"
+      - "/srv/pterodactyl/nginx/:/etc/nginx/http.d/"
+      - "/srv/pterodactyl/certs/:/etc/letsencrypt/"
+      - "/srv/pterodactyl/logs/:/app/storage/logs"
+    environment:
+      <<: [*panel-environment, *mail-environment]
+      DB_PASSWORD: *db-password
+      APP_ENV: "production"
+      APP_ENVIRONMENT_ONLY: "false"
+      CACHE_DRIVER: "redis"
+      SESSION_DRIVER: "redis"
+      QUEUE_DRIVER: "redis"
+      REDIS_HOST: "cache"
+      DB_HOST: "database"
+      DB_PORT: "3306"
+
+networks:
+  default:
+    ipam:
+      config:
+        - subnet: 172.20.0.0/16
+EOF
+
+# Step 3: Jalankan Docker kontainer
+docker-compose up -d
+
+echo " Petrodactly sudah terinstal >.< "
+echo " Akses panel anda di: http://localhost or your-server-ip"
+echo " Akses panel anda di Port 80: http://localhost:80 or your-server-ip:80"
+
+# Step 4: Buat Useradmin untuk Panel Kamu >.<
+docker-compose run --rm panel php artisan p:user:make
